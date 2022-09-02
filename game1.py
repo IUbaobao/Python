553e6c80504dd300c7a8dcbeb201bbefc508111c
IUbaobao <2868798970@qq.com>
2022-09-02 22:35:52 +0800

    猜数字小游戏

---
+++ game1.py
@@ -0,0 +1,22 @@
+import random
+
+guess=random.randint(1,100)
+# print(guess)
+
+flag=0
+a = int(input("请猜数字"))
+while flag==0:
+    if a==guess:
+        print("恭喜你，猜到了")
+        flag=1
+        break
+    elif a > guess:
+        print("猜大了")
+        a=int(input())
+
+    else:
+        print("猜小了")
+        a = int(input())
+
+
+
