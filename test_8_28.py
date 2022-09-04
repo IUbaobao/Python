2fc1fb0c4ef221a560f2f84444edc8b9aaa49791
IUbaobao <2868798970@qq.com>
2022-09-04 22:22:11 +0800

    测试

--- test_8_28.py
+++ test_8_28.py
@@ -1,12 +1,19 @@
 # 类的定义
 class test1():
+    name=None
+    age=None
+    def __int__(self):
+        self.name="小明"
+        self.age=19
+        print("自动执行的初始化")
 
     def sum(self,a):
         return a+1
 
 
 a= test1()
-print(a)
+# print(a)
+print(a.name)
 b=a.sum(1)
 print(b)
 
